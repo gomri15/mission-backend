52c41124e8e4ce858fd5b0cfe17d5b2a4842145f
Omri Golan <gomri15@gmail.com>
2019-07-17 07:13:43 +0300

    Added login route

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,9 +1,25 @@
-var express = require("express");
-var router = express.Router();
+const playerService = require('../services/playerService')
 
-router.post
-    ("/", function (req, res, next) {
+let express = require("express");
+let router = express.Router();
 
-    });
+router.post("/", async (req, res, next) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    const query = { name: username }
+    try {
+        const user = await playerService.find(id = null, query)
+
+        if (user[0].name && user[0].password === password) {
+            res.status(200).json({ message: "Successfull login" })
+        } else {
+            res.status(409).json({ message: "Username or Password are incorrect" })
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+});
 
 module.exports = router;
